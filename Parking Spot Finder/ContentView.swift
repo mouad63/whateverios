44b1c77e44c76cd232e52f14aaa92931f18edf5c
Mouad Asri <mouad.assri@gmail.com>
2024-05-23 12:34:47 +0100

    Initial Commit

---
+++ Parking Spot Finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parking Spot Finder
+//
+//  Created by Macbook Pro on 23/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world Mouad asri!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parking Spot Finder/Parking_Spot_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parking_Spot_FinderApp.swift
+//  Parking Spot Finder
+//
+//  Created by Macbook Pro on 23/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Parking_Spot_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
